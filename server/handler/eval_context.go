8e730232042e2e093ea93621950359add310fd55
alhardanaden <aden@uncountable.com>
2026-03-09 11:59:50 -0700

    Hardcoded name

--- server/handler/eval_context.go
+++ server/handler/eval_context.go
@@ -183,14 +183,13 @@ func (ec *EvalContext) PostStatus(ctx context.Context, state, message string) {
 	owner := ec.PullContext.RepositoryOwner()
 	repo := ec.PullContext.RepositoryName()
 	sha := ec.PullContext.HeadSHA()
-	base, _ := ec.PullContext.Branches()
 
 	publicURL := strings.TrimSuffix(ec.PublicURL, "/")
 	detailsURL := fmt.Sprintf("%s/details/%s/%s/%d", publicURL, owner, repo, ec.PullContext.Number())
 
 	status := github.RepoStatus{
 		State:       &state,
-		Context:     github.Ptr(fmt.Sprintf("%s: %s", ec.Options.StatusCheckContext, base)),
+		Context:     github.Ptr("Policy Bot"),
 		Description: &message,
 		TargetURL:   &detailsURL,
 	}
